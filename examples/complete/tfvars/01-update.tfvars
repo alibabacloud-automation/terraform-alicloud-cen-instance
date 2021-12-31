511ebd3b918abf4de25e4d4b8e531a78a8dcb313
liuyuan <709503583@qq.com>
2021-12-31 11:24:57 +0800

    Improves the module examples/complete

---
+++ examples/complete/tfvars/01-update.tfvars
@@ -0,0 +1,8 @@
+name        = "update-cen-instance-name"
+description = "update-cen-example"
+tags = {
+  update_test_tags = "update_tags"
+}
+cen_tags = {
+  update_cen_test_tags = "update_cen_tags"
+}
--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# ----------------------------------------------------------------------------------------------------------------------
-terraform {
-  required_version = ">= 0.12"
-}
-
 ###############
 # CEN instance
 ###############
 resource "alicloud_cen_instance" "this" {
-  name        = var.name
-  description = var.description
+  cen_instance_name = var.name
+  description       = var.description
   tags = merge(
     {
       "Name" = format("%s", var.name)
@@ -25,11 +17,10 @@ resource "alicloud_cen_instance" "this" {
 # CEN attachments
 ###############
 resource "alicloud_cen_instance_attachment" "this" {
-
   for_each                 = var.instances_attachment
   instance_id              = alicloud_cen_instance.this.id
   child_instance_id        = each.value.vpc_id
   child_instance_region_id = each.value.vpc_region_id
   child_instance_owner_id  = each.value.vpc_owner_id != "" ? each.value.vpc_owner_id : null
-
+  child_instance_type      = "VPC"
 }
