5f045f42e22cb34440031d1634e1af6b57a68a01
shanye <shanye.xzq@alibaba-inc.com>
2026-07-30 11:20:27 +0800

    fix(cen-instance): preserve update test script

--- scripts/terraform-test.sh
+++ scripts/terraform-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 version=""
+updateFolder="examples/complete"
+tfvars="tfvars/01-update.tfvars"
 f=${1}
 success=true
 # echo $f
@@ -40,15 +42,43 @@ else
         echo -e "\033[31m[ERROR]\033[0m: running terraform apply failed."
     else
         echo -e "\033[32m - apply check: success\033[0m"
-        echo ""
-        echo -e " ----> Apply Diff Checking\n"
-        terraform -chdir=$f plan -detailed-exitcode
-        if [[ $? -ne 0 ]]; then
-          success=false
-          exitCode=4
-          echo -e "\033[31m[ERROR]\033[0m: running terraform plan for checking diff failed."
+        # update & check diff
+        if [ $f == $updateFolder ] && [ -f "${updateFolder}/${tfvars}" ];then 
+          # if example is complete and has tfvars folder
+          echo ""
+          echo -e " ----> Apply Update Testing\n"
+          terraform -chdir=$f apply -auto-approve -var-file=$tfvars >/dev/null
+          if [[ $? -ne 0 ]]; then
+            success=false
+            exitCode=3
+            echo -e "\033[31m[ERROR]\033[0m: running terraform apply update failed."
+          else
+            echo -e "\033[32m - apply update check: success\033[0m"
+            echo ""
+            echo -e " ----> Apply Diff Checking\n"
+            terraform -chdir=$f plan -var-file=$tfvars -detailed-exitcode
+            if [[ $? -ne 0 ]]; then
+              success=false
+              if [[ $exitCode -eq 0 ]]; then
+                exitCode=4
+              fi
+              echo -e "\033[31m[ERROR]\033[0m: running terraform plan for checking diff failed."
+            else
+              echo -e "\033[32m - apply diff check: success\033[0m"
+            fi
+          fi
         else
-          echo -e "\033[32m - apply diff check: success\033[0m"
+          # if example is no need to update
+          echo ""
+          echo -e " ----> Apply Diff Checking\n"
+          terraform -chdir=$f plan -detailed-exitcode
+          if [[ $? -ne 0 ]]; then
+            success=false
+            exitCode=4
+            echo -e "\033[31m[ERROR]\033[0m: running terraform plan for checking diff failed."
+          else
+            echo -e "\033[32m - apply diff check: success\033[0m"
+          fi
         fi
     fi
     # destroy
